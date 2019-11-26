173d246185b5caf3cd6a6bb41dd69a7d264ed37b
Tibor den Held <tibor.denheld@ns.nl>
2019-11-26 15:52:28 +0100

    add items

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,3 +1,44 @@
+const navMobile = () => {
+    const activeClass = 'is-active';
+    const nav = document.querySelector('[data-nav="navigation"]');
+    const toggle = document.querySelector('[data-nav="toggle"]');
+    const menu = document.querySelector('[data-nav="menu"]');
+    const item = '[data-nav="item"]';
+
+    const tl = gsap.timeline({
+        defaults: {
+            paused: true,
+            ease: "power3.inOut",
+        }
+    }).fromTo(item, {
+        opacity: 0
+    }, {
+        duration: 0.5,
+        stagger: 0.07,
+        opacity: 1
+    });
+
+    toggle.onclick = () => {
+        nav.classList.toggle(activeClass);
+        toggle.classList.toggle(activeClass);
+        menu.classList.toggle(activeClass);
+
+        const tl = gsap.timeline({
+            defaults: {
+                ease: "power3.inOut",
+            }
+        }).fromTo(item, {
+            opacity: 0,
+            y: -10,
+        }, {
+            duration: 0.3,
+            stagger: 0.04,
+            opacity: 1,
+            y: 0,
+        });
+    }
+}
+
 const navDesktop = () => {
     const activeClass = 'is-active';
     const navItems = ß('.js-nav-item');
@@ -5,7 +46,7 @@ const navDesktop = () => {
 
     const removeActive = (item) => {
         item.classList.remove(activeClass);
-    };
+    }
 
     const addActive = (target, page) => {
         target.classList.add(activeClass);
@@ -36,4 +77,5 @@ const navDesktop = () => {
 
 document.addEventListener('DOMContentLoaded', () => {
     navDesktop();
+    navMobile();
 });
